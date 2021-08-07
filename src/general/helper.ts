e28ec83b90003ab388d1e22fa6a80d6b5cec641d
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-08 00:04:42 +0500

    !feat/env: added helper function

---
+++ src/general/helper.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+
+// eslint-disable-next-line import/prefer-default-export
+export const useGetDevices = () => {
+  const [audioInputDevices, setAudioInputDevices] = useState<any>([])
+  const [videoInputDevices, setVideoInputDevices] = useState<any>([])
+
+  useEffect(() => {
+    OT.getUserMedia({}).then(() => {
+      OT.getDevices((_error: any, devices: any) => {
+        const audioDevices = devices.filter((device: any) => device.kind === 'audioInput');
+        const videoDevices = devices.filter((device:any) => device.kind === 'videoInput');
+        setAudioInputDevices(audioDevices)
+        setVideoInputDevices(videoDevices);
+      })
+    })
+  }, [])
+
+  return {
+    audioInputDevices,
+    videoInputDevices,
+  }
+}
