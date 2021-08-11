6903818aa00c45d0988a44ffc130157bf5d5ebc1
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-11 12:42:59 +0500

    !fix(CT-1) : session connected

--- src/components/JoinMeeting/index.tsx
+++ src/components/JoinMeeting/index.tsx
@@ -22,10 +22,13 @@ const JoinMeetingComponent: FC <{}> = () => {
   }, [])
 
   const sessionEventHandler = {
+    connectionCreated: () => {
+      console.info('CONNECTION CREATED');
+      setConnected(true);
+    },
     streamCreated: ({ stream } : any) => {
       console.log('STREAM HAS BEEN CREATED')
       setStreams((_streams : any) => [..._streams, stream]);
-      setConnected(true);
     },
     streamDestroyed: ({ stream } : any) => {
       console.log('STREAM HAS BEEN DESTROYED');
@@ -44,14 +47,14 @@ const JoinMeetingComponent: FC <{}> = () => {
     <div>
       <h1>Welcome to the room</h1>
       <OTSession
-        apiKey="47302914"
-        sessionId="2_MX40NzMwMjkxNH5-MTYyODU5NzcxNTg1MH45QTBtcVVrQ3Q3MGpjRVUrVnlHMllVRGF-fg"
-        token="T1==cGFydG5lcl9pZD00NzMwMjkxNCZzaWc9MzU4ZDFiYTg0NjY4ODhlN2FkZGU3NTI3ZGNkYmM3MmU0MTI3OTUxMzpzZXNzaW9uX2lkPTJfTVg0ME56TXdNamt4Tkg1LU1UWXlPRFU1TnpjeE5UZzFNSDQ1UVRCdGNWVnJRM1EzTUdwalJWVXJWbmxITWxsVlJHRi1mZyZjcmVhdGVfdGltZT0xNjI4NTk3NzU2Jm5vbmNlPTAuNDg1MDkxMDMzODc4OTI0Mjcmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTYzMTE4OTc1NCZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ=="
+        apiKey="47000694"
+        sessionId="2_MX40NzAwMDY5NH5-MTYyODU5MzY3Njg4Nn5rSGtjUmlPQzBSVVgvRWJHZlEwZjVUTkR-fg"
+        token="T1==cGFydG5lcl9pZD00NzAwMDY5NCZzaWc9ZDc2MmY4MjY2MWQ1MDg4Y2I5ZTBjZjA4OWYyODJhM2YxZmQzNDEzNTpzZXNzaW9uX2lkPTJfTVg0ME56QXdNRFk1Tkg1LU1UWXlPRFU1TXpZM05qZzRObjVyU0d0alVtbFBRekJTVlZndlJXSkhabEV3WmpWVVRrUi1mZyZjcmVhdGVfdGltZT0xNjI4NTkzOTA1Jm5vbmNlPTAuOTUzODUyNjg0ODAyNTI0NCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjMxMTg1OTAzJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9"
         eventHandlers={sessionEventHandler}
         onError={onError}
       >
         {error ? <div>{error}</div> : null}
-        { !connected && (
+        {connected && (
         <>
           <ConnectionStatusComponent connection={connected} />
           <PublisherComponent />
